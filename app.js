ad6fd3888d86a4840ff8496ab85b95b8e2973f0b
Philip Kouchner <philipk179@gmail.com>
2020-04-30 12:34:14 -0700

    moment js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
 
+    var localDay = moment();
+    var nextWeek = [];
+    for (var i = 0; i < 5; i++) {
 
+        nextWeek.push(localDay.add(1, "day").format("MM/DD/YYYY"));
+    }
+    localDay = moment();
     var apiKey = "7f9d2254f4ebfa05b38921a6dcf77731";
     var inputUser = "";
 
@@ -17,11 +23,12 @@ $(document).ready(function () {
 
         }).then(function (res) {
 
-            var nameCity = res.name;
+            console.log(res);
+            var nameCity = res.name + " " + `(${localDay.format("MM/DD/YYYY")})`;
             var cloud = res.weather[0].description;
             var temp = res.main.temp;
 
-            $("#presentWeather").append(`<h1 class="tag">${nameCity}</h1>`);
+            $("#presentWeather").append(`<label class="lead tag"><i class="fas fa-cloud">${nameCity}</i></label>`);
             $("#presentWeather").append(`<h2 class="tag">${cloud}</h2>`);
             $("#presentWeather").append(`<h3 class="tag">${temp}</h3>`);
         });
