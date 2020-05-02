351bd48e4d778b4b2d4ca64fba39a66f44942e30
Philip Kouchner <philipk179@gmail.com>
2020-05-02 14:00:02 -0700

    style

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ $(document).ready(function () {
         $("#presentWeather").html("");
         $("#append").html("");
         renderCity(inputUser, apiKey);
-
-
     });
 
     function renderCity(inputUser, key) {
@@ -54,19 +52,18 @@ $(document).ready(function () {
 
             }).then(function (res) {
 
-
-                var t = moment(arrWeather[0].time).format("MM/DD/YYYY");
-                $("#presentWeather").append(`<div class="text-primary divWeather">${arrWeather[0].name + " " + t}<div><img id="icon" src="http://openweathermap.org/img/wn/${arrWeather[0].icon}.png"/></div></div>`);
+                var time = moment(arrWeather[0].time).format("MM/DD/YYYY");
+                $("#presentWeather").append(`<div class="text-primary divWeather">${arrWeather[0].name + " " + time}<div><img id="icon" src="http://openweathermap.org/img/wn/${arrWeather[0].icon}.png"/></div></div>`);
                 $("#presentWeather").append(`<div class="text-info divWeather">Temperature: ${arrWeather[0].tempeture}</div>`);
                 $("#presentWeather").append(`<div class="text-info divWeather">Humidity: ${arrWeather[0].humidity}</div>`);
                 $("#presentWeather").append(`<div class="text-info divWeather">Wind Speed: ${arrWeather[0].windSpeed}</div>`);
-                $("#presentWeather").append(`<div class="text-info divWeather">Uv: ${res.value}</div>`);
+                $("#presentWeather").append(`<div class="text-info divWeather">UV: ${res.value}</div>`);
 
                 for (var i = 1; i < arrWeather.length; i++) {
-                    var t = moment(arrWeather[i].time).format("MM/DD/YYYY");
+                    var time = moment(arrWeather[i].time).format("MM/DD/YYYY");
 
-                    $("#append").append(`<div class="card text-white bg-primary mb-2" id="append" style="max-width: 18rem;">
-                    <div class="card-header">${t}
+                    $("#append").append(`<div class="card text-white bg-primary mb-2" id="append" style="max-width: 10rem;">
+                    <div class="card-header">${time}
                     <img id="icon" src="http://openweathermap.org/img/wn/${arrWeather[i].icon}.png"/></div>
                     <div class="card-body">
                         <h5 class="card-title">Humidity:${arrWeather[i].humidity}</h5>
