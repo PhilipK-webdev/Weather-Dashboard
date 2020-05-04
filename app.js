594a41f85f6de927b3b419e79fef5db214cb455c
Philip Kouchner <philipk179@gmail.com>
2020-05-04 09:34:42 -0700

    5 day forecast

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ $(document).ready(function () {
     var latitude;
     var longitude;
     var arrWeather;
-    var uv;
+    var arrValueInput = [];
+
+
 
     setArray();
 
@@ -15,10 +17,18 @@ $(document).ready(function () {
             event.preventDefault();
             $(".display-5").show();
             inputUser = $("#textCityName").val();
-            console.log(inputUser);
-            inputUser = inputUser.substr(0, 1).toUpperCase() + inputUser.substr(1);
-            $(".list-group").prepend(` <li class="list-group-item text-primary">${inputUser}</li>`);
 
+            if (!arrValueInput.includes(inputUser.toLocaleLowerCase())) {
+
+                arrValueInput.push(inputUser.toLocaleLowerCase());
+                inputUser = inputUser.substr(0, 1).toUpperCase() + inputUser.substr(1);
+                $(".list-group").prepend(` <li class="list-group-item text-primary">${inputUser}</li>`);
+
+
+            } else {
+
+                alert("WRONG");
+            }
 
             $("#textCityName").val("");
             $("#presentWeather").html("");
@@ -56,10 +66,40 @@ $(document).ready(function () {
         $(".display-5").hide();
     }
 
-
-    function renderLocalStorage() {
+    function display(arrWeather) {
 
         var time = moment(arrWeather[0].time).format("MM/DD/YYYY");
+        $("#presentWeather").append(`<div class="text-primary">${arrWeather[0].name + " " + time}<div><img src="http://openweathermap.org/img/wn/${arrWeather[0].icon}.png"/></div></div>`);
+        $("#presentWeather").append(`<div class="text-info">Temperature: ${arrWeather[0].tempeture} F</div>`);
+        $("#presentWeather").append(`<div class="text-info">Humidity: ${arrWeather[0].humidity} %</div>`);
+        $("#presentWeather").append(`<div class="text-info">Wind Speed: ${arrWeather[0].windSpeed} MPH</div>`);
+        $("#presentWeather").append(`<div class="text-info"><p id="uv">UV: ${arrWeather[6]}</p></div>`);
+
+
+        var color = arrWeather[5] > 7 ? "red" : "blue"
+
+        $("#uv").addClass(color);
+
+
+        for (var i = 1; i < arrWeather.length - 1; i++) {
+            var time = moment(arrWeather[i].time).format("MM/DD/YYYY");
+
+            $("#append").append(`<div class="card text-white bg-primary mb-2 mr-2 ml-2" style="max-width: 10rem;">
+            <div class="card-header">${time}
+            <img id="icon" src="http://openweathermap.org/img/wn/${arrWeather[i].icon}.png"/></div>
+            <div class="card-body">
+                <p class="card-title">Humidity:${arrWeather[i].humidity} %</p>
+                <p class="card-text">Temperature: <br>${arrWeather[i].tempeture} F</p>
+            </div>
+        </div>`)
+        }
+
+    }
+
+    function renderLocalStorage() {
+        display(arrWeather);
+
+        /* var time = moment(arrWeather[0].time).format("MM/DD/YYYY");
         $("#presentWeather").append(`<div class="text-primary">${arrWeather[0].name + " " + time}<div><img id="icon" src="http://openweathermap.org/img/wn/${arrWeather[0].icon}.png"/></div></div>`);
         $("#presentWeather").append(`<div class="text-info">Temperature: ${arrWeather[0].tempeture} F</div>`);
         $("#presentWeather").append(`<div class="text-info">Humidity: ${arrWeather[0].humidity} %</div>`);
@@ -86,7 +126,7 @@ $(document).ready(function () {
                 <p class="card-text">Temperature: <br>${arrWeather[i].tempeture} F</p>
             </div>
         </div>`)
-        }
+        } */
 
     }
     function renderCity(inputUser, key, arrWeather) {
@@ -99,12 +139,26 @@ $(document).ready(function () {
 
         }).then(function (res) {
 
+            console.log(res);
+
             latitude = res.city.coord.lat.toString();
             longitude = res.city.coord.lon.toString();
             arrWeather = [];
+
+            var Weather = {
+                name: res.city.name,
+                tempeture: res.list[0].main.temp,
+                humidity: res.list[0].main.humidity,
+                windSpeed: res.list[0].wind.speed,
+                time: res.list[0].dt_txt,
+                icon: res.list[0].weather[0].icon
+            };
+
+            arrWeather.push(Weather);
+            console.log(arrWeather);
             for (var i = 3; i < res.list.length; i += 8) {
 
-                var Weather = {
+                Weather = {
                     name: res.city.name,
                     tempeture: res.list[i].main.temp,
                     humidity: res.list[i].main.humidity,
@@ -124,38 +178,38 @@ $(document).ready(function () {
 
             }).then(function (res) {
 
-
-
-
                 arrWeather.push(res.value);
-                var time = moment(arrWeather[0].time).format("MM/DD/YYYY");
-                $("#presentWeather").append(`<div class="text-primary">${arrWeather[0].name + " " + time}<div><img src="http://openweathermap.org/img/wn/${arrWeather[0].icon}.png"/></div></div>`);
-                $("#presentWeather").append(`<div class="text-info">Temperature: ${arrWeather[0].tempeture} F</div>`);
-                $("#presentWeather").append(`<div class="text-info">Humidity: ${arrWeather[0].humidity} %</div>`);
-                $("#presentWeather").append(`<div class="text-info">Wind Speed: ${arrWeather[0].windSpeed} MPH</div>`);
-                $("#presentWeather").append(`<div class="text-info"><p id="uv">UV: ${arrWeather[5]}</p></div>`);
-
-                if (res.value > 7) {
-
-                    $("#uv").addClass("red");
-                } else {
-
-                    $("#uv").addClass("blue");
-                }
-
-                for (var i = 1; i < arrWeather.length - 1; i++) {
-                    var time = moment(arrWeather[i].time).format("MM/DD/YYYY");
-
-                    $("#append").append(`<div class="card text-white bg-primary mb-2 mr-2 ml-2" style="max-width: 10rem;">
-                    <div class="card-header">${time}
-                    <img id="icon" src="http://openweathermap.org/img/wn/${arrWeather[i].icon}.png"/></div>
-                    <div class="card-body">
-                        <p class="card-title">Humidity:${arrWeather[i].humidity} %</p>
-                        <p class="card-text">Temperature: <br>${arrWeather[i].tempeture} F</p>
-                    </div>
-                </div>`)
-                }
+                display(arrWeather)
+                /*  var time = moment(arrWeather[0].time).format("MM/DD/YYYY");
+                 $("#presentWeather").append(`<div class="text-primary">${arrWeather[0].name + " " + time}<div><img src="http://openweathermap.org/img/wn/${arrWeather[0].icon}.png"/></div></div>`);
+                 $("#presentWeather").append(`<div class="text-info">Temperature: ${arrWeather[0].tempeture} F</div>`);
+                 $("#presentWeather").append(`<div class="text-info">Humidity: ${arrWeather[0].humidity} %</div>`);
+                 $("#presentWeather").append(`<div class="text-info">Wind Speed: ${arrWeather[0].windSpeed} MPH</div>`);
+                 $("#presentWeather").append(`<div class="text-info"><p id="uv">UV: ${arrWeather[5]}</p></div>`);
+ 
+                 if (arrWeather[5] > 7) {
+ 
+                     $("#uv").addClass("red");
+                 } else {
+ 
+                     $("#uv").addClass("blue");
+                 }
+ 
+                 for (var i = 1; i < arrWeather.length - 1; i++) {
+                     var time = moment(arrWeather[i].time).format("MM/DD/YYYY");
+ 
+                     $("#append").append(`<div class="card text-white bg-primary mb-2 mr-2 ml-2" style="max-width: 10rem;">
+                     <div class="card-header">${time}
+                     <img id="icon" src="http://openweathermap.org/img/wn/${arrWeather[i].icon}.png"/></div>
+                     <div class="card-body">
+                         <p class="card-title">Humidity:${arrWeather[i].humidity} %</p>
+                         <p class="card-text">Temperature: <br>${arrWeather[i].tempeture} F</p>
+                     </div>
+                 </div>`)
+                 }
+  */
                 window.localStorage.setItem("city", JSON.stringify(arrWeather));
+
             });
 
 
